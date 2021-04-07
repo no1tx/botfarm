d94b2601d2d1e9f5190ac157618fd4fab4282703
Pavel Isaev <no1@no1sg.ru>
2021-04-07 16:01:43 +0300

    graceful shutdown try

--- app.py
+++ app.py
@@ -2,12 +2,22 @@ import logging
 import asyncio
 from process import run_bot_farm
 
-LOG_FORMAT = ('%(asctime)s,%(msecs)d %(levelname)s: %(message)s')
+LOG_FORMAT = '%(asctime)s,%(msecs)d %(levelname)s: %(message)s'
 LOGGER = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG, format=LOG_FORMAT)
 
 if __name__ == '__main__':
     event_loop = asyncio.new_event_loop()
     asyncio.set_event_loop(event_loop)
-    event_loop.create_task(run_bot_farm())
-    event_loop.run_forever()
+    try:
+        event_loop.create_task(run_bot_farm())
+        event_loop.run_forever()
+    except KeyboardInterrupt:
+        LOGGER.log(logging.INFO, msg="Catched keyboard interrupt...")
+
+    finally:
+        tasks = [t for t in asyncio.all_tasks() if t is not
+                 asyncio.current_task()]
+        [task.cancel() for task in tasks]
+        event_loop.close()
+        LOGGER.log(logging.INFO, msg="Shutdown...")
